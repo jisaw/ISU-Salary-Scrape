99e7325f7e508ca949bb903e03380220f658a4e0
Jacob <jisawyer@iastate.edu>
2015-01-08 11:45:22 -0600

    good stopping place

--- main.go
+++ main.go
@@ -2,7 +2,8 @@ package main
 
 import "fmt"
 import "net/http"
-
+import "golang.org/x/net/html"
+import "strings"
 import "io/ioutil"
 
 func main() {
@@ -13,11 +14,26 @@ func main() {
 		return
 	} else {
 	defer resp.Body.Close()
-	doc, err := ioutil.ReadAll(resp.Body)
+	//doc, err := ioutil.ReadAll(resp.Body)
+	data, err := html.Parse(resp)
 	if err != nil {
 		fmt.Println(err)
-		return
+	} 
+	fmt.Println("[INFO] Parsing out data")
+	var f func(n *html.Node)
+	f = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, a := range n.Attr {
+				if a.Key == "td" {
+					fmt.Println(a.Val)
+					break
+				}
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
 	}
-	fmt.Println("%s\n", string(doc))
+	f(data)
 	}
 }
